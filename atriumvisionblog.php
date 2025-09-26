0df0ab0bfed92fe9927ea5d527e14170976033c0
Adam Mańko <adam.maniek247@gmail.com>
2025-09-26 23:42:11 +0200

    Basic class configuration

--- atriumvisionblog.php
+++ atriumvisionblog.php
@@ -9,22 +9,21 @@ use PrestaShop\PrestaShop\Adapter\SymfonyContainer;
 use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
 
 /**
- * Modernmodule - starter skeleton for PrestaShop 8/9, Symfony-based.
+ * Atriumvisionblog
  */
-class Modernmodule extends Module
+class Atriumvisionblog extends Module
 {
     public function __construct()
     {
-        $this->name = 'modernmodule';
+        $this->name = 'atriumvisionblog';
         $this->tab = 'administration';
         $this->version = '0.1.0';
-        $this->author = 'Your Company';
+        $this->author = 'Adam Mańko';
         $this->need_instance = 0;
         $this->bootstrap = true;
-        $this->displayName = $this->trans('Modern module skeleton', [], 'Modules.Modernmodule.Admin');
-        $this->description = $this->trans('Symfony-first module boilerplate for PrestaShop 8/9', [], 'Modules.Modernmodule.Admin');
+        $this->displayName = $this->trans('Blog', [], 'Modules.Atriumvisionblog.Admin');
+        $this->description = $this->trans('Create blog on your PrestaShop', [], 'Modules.Atriumvisionblog.Admin');
 
-        // Support PS 8 and 9
         $this->ps_versions_compliancy = ['min' => '8.0.0', 'max' => '9.99.99'];
 
         parent::__construct();
@@ -33,22 +32,22 @@ class Modernmodule extends Module
     /**
      * Add a back-office tab that points to our Symfony route.
      */
-    public function getTabs()
-    {
-        return [
-            [
-                'class_name' => 'AdminModernmoduleConfiguration',
-                'visible' => true,
-                'name' => [
-                    'en' => 'Modern module',
-                    'pl' => 'Nowoczesny moduł',
-                ],
-                'parent_class_name' => 'CONFIGURE',
-                'icon' => 'build',
-                // Route is wired via _legacy_link in config/routes.yml
-            ],
-        ];
-    }
+    // public function getTabs()
+    // {
+    //     return [
+    //         [
+    //             'class_name' => 'AdminModernmoduleConfiguration',
+    //             'visible' => true,
+    //             'name' => [
+    //                 'en' => 'Modern module',
+    //                 'pl' => 'Nowoczesny moduł',
+    //             ],
+    //             'parent_class_name' => 'CONFIGURE',
+    //             'icon' => 'build',
+    //             // Route is wired via _legacy_link in config/routes.yml
+    //         ],
+    //     ];
+    // }
 
     public function install()
     {
@@ -63,10 +62,8 @@ class Modernmodule extends Module
 
     public function getContent()
     {
-        // Redirect "Configure" button to our Symfony controller
-        $router = SymfonyContainer::getInstance()->get('router');
-        $url = $router->generate('modernmodule_configuration');
-        Tools::redirectAdmin($url);
+        $route = $this->get('router')->generate('atriumvisionblog_configuration');
+        Tools::redirectAdmin($route);
     }
 
     /** Example: add module assets on FO */
@@ -74,7 +71,7 @@ class Modernmodule extends Module
     {
         // Example asset (not required)
         // $this->context->controller->registerStylesheet(
-        //     'modernmodule-front',
+        //     'Atriumvisionblog-front',
         //     'modules/'.$this->name.'/views/assets/front.css',
         //     ['priority' => 50]
         // );
